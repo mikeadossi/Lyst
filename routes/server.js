2326fd47b9ae82bab1e721c77663b497fbbcc164
mikeadossi <mikeadossi@gmail.com>
2017-05-09 16:39:22 -0700

    pushing to db

--- routes/server.js
+++ routes/server.js
@@ -2,11 +2,22 @@ var express = require("express");
 var router = express.Router();
 var Queries = require("../database/queries.js");
 
+
 router.get('/', function(req, res, next) {
   Queries.getAll().then(function(projects) {
     res.render("index", {
-      title: 'To Drop',
       projects: projects
+    })
+  })
+})
+
+router.post('/insert-task', function(req, res, next) {
+  Queries.addTask(req.body.user_entry).then(function(projects) {
+    console.log('yyy')
+    Queries.getAll().then(function(projects) {
+      res.render("index", {
+        projects: projects
+      })
     })
   })
 })
