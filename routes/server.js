82e0074df0d6de4c6d9c3afc4c560ae91ac4b00d
mikeadossi <mikeadossi@gmail.com>
2017-05-10 09:44:00 -0700

    addTask working

--- routes/server.js
+++ routes/server.js
@@ -13,22 +13,13 @@ router.get('/', function(req, res, next) {
 
 router.post('/insert-task', function(req, res, next) {
 
-  if(req.body.delete_item){
     Queries.addTask(req.body.user_entry).then(function(projects) {
       Queries.getAll().then(function(projects) {
         res.render("index", {
           projects: projects
         })
       })
-    })} else {
-      Queries.addTask(req.body.user_entry).then(function(projects) {
-        Queries.getAll().then(function(projects) {
-          res.render("index", {
-            projects: projects
-          })
-        })
-      })
-    }
+    })
 
 })
 
@@ -39,3 +30,20 @@ router.post('/insert-task', function(req, res, next) {
 
 
 module.exports = router
+
+// if(req.body.delete_item){
+//   Queries.addTask(req.body.user_entry).then(function(projects) {
+//     Queries.getAll().then(function(projects) {
+//       res.render("index", {
+//         projects: projects
+//       })
+//     })
+//   })} else {
+//     Queries.addTask(req.body.user_entry).then(function(projects) {
+//       Queries.getAll().then(function(projects) {
+//         res.render("index", {
+//           projects: projects
+//         })
+//       })
+//     })
+//   }
